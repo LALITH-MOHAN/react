211c55952b22f47e3814fc6215c146e107ae422d
LALITH-MOHAN <lalith22004@gmail.com>
2025-05-17 20:07:27 +0530

    added lazy component

--- Route/src/App.jsx
+++ Route/src/App.jsx
@@ -1,26 +1,37 @@
-import React from "react"
-import {BrowserRouter,Route,Routes,Link} from 'react-router-dom'
-import Home from "./Pages/home"
-import About from "./Pages/about"
-import Contact from "./Pages/contact"
-import  Userprofile from "./Pages/Userprofle"
+import React, { Suspense, lazy } from "react";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import Home from "./Pages/home";
+import Contact from "./Pages/contact";
+import Userprofile from "./Pages/Userprofle";
+
+
+// 👇 Lazy load About page with simulated 5s delay
+const About = lazy(() => import("./Pages/about"));
+
 function App() {
   return (
     <BrowserRouter>
-    <nav style={{ marginBottom: '20px' }}>
-      <Link to="/">Home</Link> |{" "}
-      <Link to="/about">About</Link> |{" "}
-      <Link to="/contact">Contact</Link>
-    </nav>
+      <nav style={{ marginBottom: '20px' }}>
+        <Link to="/">Home</Link> |{" "}
+        <Link to="/about">About</Link> |{" "}
+        <Link to="/contact">Contact</Link>
+      </nav>
 
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/user/:id" element={<Userprofile />} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/about"
+          element={
+            <Suspense fallback={<h1>LOADING ABOUT.....</h1>}>
+              <About />
+            </Suspense>
+          }
+        />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/user/:id" element={<Userprofile />} />
       </Routes>
-  </BrowserRouter>
-  )
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
